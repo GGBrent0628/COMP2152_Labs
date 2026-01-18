4802531e61c7c181d4aea6e564aacf5b1a2faca8
GGBrent0628 <brentjoshua.samson@georgebrown.ca>
2026-01-18 00:07:44 -0500

    Lab1.py Complete

---
+++ Week01/lab1.py
@@ -0,0 +1,33 @@
+# Sample Coding Question 01 Week 01
+# Brent Joshua Samson
+
+
+#Question 2: Defining an Array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: 
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Original expression:
+# e = a - b ** c // d + a % c
+
+# Fully-Bracketed Version:
+e = (a - ((b ** c) // d)) + (a % c)
+
+
+# Question 4: 
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+# Question 5: 
+# User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age:", userAge)
+
